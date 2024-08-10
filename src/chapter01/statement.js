ce3eda9e1ae6cf790aea719183555a6d46ef9e8e
MinhyukWoo <minhyukwoo.dev@gmail.com>
2024-08-11 00:06:05 +0900

    Extract inner function totalAmount

--- src/chapter01/statement.js
+++ src/chapter01/statement.js
@@ -1,16 +1,23 @@
 function statement(invoice, plays) {
-  let totalAmount = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for (const perf of invoice.performances) {
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
-    totalAmount += amountFor(perf);
   }
-  result += `총액: ${usd(totalAmount)}\n`;
+  let myTotalAmount = totalAmount();
+  result += `총액: ${usd(myTotalAmount)}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
+
+  function totalAmount() {
+    let result = 0;
+    for (const perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+    return result;
+  }
 
   function totalVolumeCredits() {
     let result = 0;
