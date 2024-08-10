fc86440e3a41b343310d375cbeed0cbf8eba4e54
MinhyukWoo <minhyukwoo.dev@gmail.com>
2024-08-10 15:26:20 +0900

    Rename thisAmount to result

--- src/chapter01/statement.js
+++ src/chapter01/statement.js
@@ -29,22 +29,22 @@ function statement(invoice, plays) {
   function amountFor(play, perf) {
     switch (play.type) {
       case "tragedy":
-        thisAmount = 40000;
+        result = 40000;
         if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
+          result += 1000 * (perf.audience - 30);
         }
         break;
       case "comedy":
-        thisAmount = 30000;
+        result = 30000;
         if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
+          result += 10000 + 500 * (perf.audience - 20);
         }
-        thisAmount += 300 * perf.audience;
+        result += 300 * perf.audience;
         break;
       default:
         throw new Error(`알 수 없는 장르: ${play.type}`);
     }
-    return thisAmount;
+    return result;
   }
 }
 
