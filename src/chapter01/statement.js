037a89dde8aecd48ada9fe2eea0b3428d6c9cbcf
MinhyukWoo <minhyukwoo.dev@gmail.com>
2024-08-11 22:08:57 +0900

    Replace totalAmount loop with reduce

--- src/chapter01/statement.js
+++ src/chapter01/statement.js
@@ -50,11 +50,7 @@ function statement(invoice, plays) {
   }
 
   function totalAmount(data) {
-    let result = 0;
-    for (const perf of data.performances) {
-      result += perf.amount;
-    }
-    return result;
+    return data.performances.reduce((total, perf) => total + perf.amount, 0);
   }
 
   function totalVolumeCredits(data) {
