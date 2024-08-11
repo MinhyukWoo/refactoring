1a59feab537db200e5900488e36a173128b7f7e8
MinhyukWoo <minhyukwoo.dev@gmail.com>
2024-08-11 17:55:02 +0900

    Change rednerPlainText declaration

--- src/chapter01/statement.js
+++ src/chapter01/statement.js
@@ -1,15 +1,17 @@
 function statement(invoice, plays) {
-  return renderPlainText(invoice);
+  return renderPlainText(invoice, plays);
+}
 
-  function renderPlainText(invoice) {
-    let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-    for (const perf of invoice.performances) {
-      result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-    }
-    result += `총액: ${usd(totalAmount())}\n`;
-    result += `적립 포인트: ${totalVolumeCredits()}점\n`;
-    return result;
+function renderPlainText(invoice, plays) {
+  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+  for (const perf of invoice.performances) {
+    result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${
+      perf.audience
+    }석)\n`;
   }
+  result += `총액: ${usd(totalAmount())}\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+  return result;
 
   function totalAmount() {
     let result = 0;
